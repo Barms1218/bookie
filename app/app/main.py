bbbd57b3d04cf9c429d811064f10aa0ffc8f7e53
Branden Arms <branden22894@gmail.com>
2026-03-29 10:54:43 -0600

    Started making database models

--- app/app/main.py
+++ app/app/main.py
@@ -3,9 +3,10 @@ from fastapi.datastructures import URL
 from app.config import settings
 from app.services import GoogleBooksService
 from app.lifespan import lifespan
-
+import uvicorn
 
 app = FastAPI(lifespan=lifespan)
+    
 
 def get_google_service():
     return GoogleBooksService(api_key=str(settings.google_api_key), client=app.state.http_client)
@@ -19,5 +20,6 @@ async def search_books(term: str, service: GoogleBooksService = Depends(get_goog
     data = await service.get_book_with_term(term)
     return {"data": data }
 
-
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
 
---
+++ app/database.py
@@ -0,0 +1,38 @@
+from typing import List
+from typing import Optional
+from sqlalchemy import ForeignKey
+from sqlalchemy import String, JSON, Float
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import Relationship
+
+class Base(DeclarativeBase):
+    pass
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    email: Mapped[str] = mapped_column(String(30))
+
+class Book(Base):
+    __tablename__ = "books"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    isbn: Mapped[str] = mapped_column(JSON)
+    title: Mapped[str] = mapped_column(String(30))
+    authors: Mapped[List[str]] = mapped_column(JSON)
+    page_count: Mapped[int] = mapped_column(JSON)
+
+
+class UserBook(Base):
+
+
+class Journals(Base):
+    __tablename__ = "journals"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    book_id: Mapped[int] = mapped_column(ForeignKey("book.id"))
+
